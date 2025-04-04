435a111953d030e83097db2f21321db27c05c9cc
Nack <77537263+nack098@users.noreply.github.com>
2025-04-05 00:37:33 +0700

    fix(main) - fixing naming
    Don't forget to change the subsystem in visual studio project settings to windows subsystem!

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <Windows.h>
 
-int __stdcall WinMain(
+int WINAPI WinMain(
     _In_        HINSTANCE   hInstance,
     _In_opt_    HINSTANCE   hPrevInstance,
     _In_        char        *lpCmdLine,
     _In_        int         nShowCmd
 )
 {
-    OutputDebugStringW(L"Hello, World\n");
+    OutputDebugStringW(L"Hello, World!\n");
     return ERROR_SUCCESS;
 }
