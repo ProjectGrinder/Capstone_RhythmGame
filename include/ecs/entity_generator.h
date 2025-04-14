8357a9a2f999125ff493df052cd05061bd450183
Summation <wachirawichkanil@gmail.com>
2025-04-14 10:50:43 +0700

    fix(ecs) - make entity_generator not a singleton

--- include/ecs/entity_generator.h
+++ include/ecs/entity_generator.h
@@ -1,19 +1,13 @@
 #pragma once
-
+#include <cstdint>
 namespace ECS
 {
     using Entity = uint32_t;
     class EntityGenerator
     {
     private:
-        static EntityGenerator _instance;
         Entity _next_entity_id = 0;
-        EntityGenerator() = default;
     public:
-        static EntityGenerator& instance()
-        {
-            return (_instance);
-        }
         Entity generate_entity()
         {
             return (_next_entity_id++);
