33a9d81c7a4ea169de4284281f16e0fa51d40b4c
Nack <77537263+nack098@users.noreply.github.com>
2025-04-26 23:10:21 +0700

    fix(game_test): misaligned number

--- test/game_test.cpp
+++ test/game_test.cpp
@@ -27,8 +27,8 @@ TEST(Game, change_scene_test)
     EXPECT_EQ(value, 1) << "Game::change_scene() returned incorrect value(it should be able to call and set outside value ie. impure function)[Expect: 1, Actual: " << value << "]";
 
     System::Game::change_scene(test_func2);
-    EXPECT_EQ(value, 2) << "Game::change_scene() returned incorrect value(it should be able to call and set outside value ie. impure function)[Expect: 1, Actual: " << value << "]";
+    EXPECT_EQ(value, 2) << "Game::change_scene() returned incorrect value(it should be able to call and set outside value ie. impure function)[Expect: 2, Actual: " << value << "]";
 
     System::Game::change_scene(test_func3);
-    EXPECT_EQ(value, 3) << "Game::change_scene() returned incorrect value(it should be able to call and set outside value ie. impure function)[Expect: 1, Actual: " << value << "]";
+    EXPECT_EQ(value, 3) << "Game::change_scene() returned incorrect value(it should be able to call and set outside value ie. impure function)[Expect: 3, Actual: " << value << "]";
 }
