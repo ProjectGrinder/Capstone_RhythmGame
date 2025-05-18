d4840309e724dcacd593bbe3202b6eed15448810
Summation <wachirawichkanil@gmail.com>
2025-05-18 18:15:30 +0700

    feat(asset-manager) - use state machine for png

--- include/utils/load_png.h
+++ include/utils/load_png.h
@@ -6,6 +6,10 @@
 
 // Courtesy of https://evanhahn.com/worlds-smallest-png/
 #define SMALLEST_PNG_SIZE 67
+#define PNG_SIGNATURE_SIZE 8
+#define PNG_CHUNK_LENGTH_SIZE 4
+#define PNG_CHUNK_TYPE_SIZE 4
+#define PNG_CHUNK_CRC_SIZE 4
 
 namespace Utils
 {
@@ -49,6 +53,15 @@ namespace Utils
         }
     };
 
+    enum _png_parse_state
+    {
+        VERIFY_HEADER,
+        READ_CHUNK_LENGTH,
+        READ_CHUNK_TYPE,
+        READ_CHUNK_DATA,
+        READ_CHUNK_CRC
+    };
+
     /// <summary>
     /// Loads a PNG file from the specified path into memory.
     /// </summary>
@@ -70,14 +83,92 @@ namespace Utils
             return false;
         }
 
-        // Verify PNG Signature
-        static const uint8_t png_signature[8] = { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
-        if (!std::equal(buffer.begin(), buffer.begin() + 8, png_signature))
+        _png_parse_state state = VERIFY_HEADER;
+
+        std::vector<uint8_t> png_signature = 
+        {
+            0x89, 0x50, 0x4E, 0x47,
+            0x0D, 0x0A, 0x1A, 0x0A
+        };
+
+        uint32_t counter = 0;
+        uint32_t length = 0;
+        uint32_t chunk_type = 0;
+        uint32_t chunk_crc = 0;
+        std::vector<uint8_t> chunk_data;
+
+        for (auto byte : buffer)
+        {
+            switch (state)
+            {
+            case VERIFY_HEADER:
+                if (byte != png_signature[counter])
+                {
+                    return false;
+                }
+                counter++;
+                if (counter == png_signature.size())
+                {
+                    state = READ_CHUNK_LENGTH;
+                    counter = 0;
+                    length = 0;
+                }
+                break;
+            case READ_CHUNK_LENGTH:
+                length = (length << 8) | byte;
+                counter++;
+                if (counter == PNG_CHUNK_LENGTH_SIZE)
+                {
+                    state = READ_CHUNK_TYPE;
+                    counter = 0;
+                }
+                break;
+            case READ_CHUNK_TYPE:
+                chunk_type = (chunk_type << 8) | byte;
+                counter++;
+                if (counter == PNG_CHUNK_TYPE_SIZE)
+                {
+                    state = READ_CHUNK_DATA;
+                    counter = 0;
+                    chunk_data.clear();
+                    chunk_data.resize(length);
+                }
+                break;
+            case READ_CHUNK_DATA:
+                chunk_data[counter] = byte;
+                counter++;
+                if (counter == length)
+                {
+                    state = READ_CHUNK_CRC;
+                    counter = 0;
+                }
+                break;
+            case READ_CHUNK_CRC:
+                chunk_crc = (chunk_crc << 8) | byte;
+                counter++;
+                if (counter == PNG_CHUNK_CRC_SIZE)
+                {
+                    state = VERIFY_HEADER;
+                    counter = 0;
+                    //TODO: CRC check
+                    //TODO: Process chunk data
+                }
+            }
+        }
+
+        if (state != VERIFY_HEADER)
+        {
+            return false;
+        }
+        if (counter != 0)
+        {
+            return false;
+        }
+        if (length != 0) // final IEND chunk has length 0
         {
             return false;
         }
 
-        // Chunking logic
-
+        return true;
     }
 }
