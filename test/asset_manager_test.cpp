4f698910ab1e0b5ba4d2222a513e859a02f22a8e
Summation <wachirawichkanil@gmail.com>
2025-05-28 20:51:17 +0700

    test(asset-manager) - finish tests

--- test/asset_manager_test.cpp
+++ test/asset_manager_test.cpp
@@ -10,6 +10,13 @@ struct DummyAsset
     DummyAsset(const std::string& n) : name(n) {}
 };
 
+struct SecondDummyAsset
+{
+    std::string name;
+    int value;
+    SecondDummyAsset(const std::string& n) : name(n), value(0) {}
+};
+
 // Override the load method temporarily by specializing _AssetStore
 namespace System 
 {
@@ -18,7 +25,15 @@ namespace System
     {
         auto asset = std::make_shared<DummyAsset>(path);
         _assets[path] = asset;
-        return (asset);
+        return asset;
+    }
+
+    template <>
+    std::shared_ptr<SecondDummyAsset> AssetManager::_AssetStore<SecondDummyAsset>::load(const std::string& path)
+    {
+        auto asset = std::make_shared<SecondDummyAsset>(path);
+        _assets[path] = asset;
+        return asset;
     }
 }
 
@@ -50,12 +65,70 @@ TEST_F(AssetManagerTest, get_same_asset_test)
     ASSERT_EQ(asset1, asset2);
 }
 
-TEST_F(AssetManagerTest, unload_asset_test) {
+TEST_F(AssetManagerTest, get_different_asset_test) 
+{
+    auto asset1 = AssetManager::instance().get<DummyAsset>("asset_one");
+    auto asset2 = AssetManager::instance().get<SecondDummyAsset>("asset_two");
+    
+    ASSERT_NE(asset1, nullptr);
+    ASSERT_NE(asset2, nullptr);
+    EXPECT_EQ(asset1->name, "asset_one");
+    EXPECT_EQ(asset2->name, "asset_two");
+}
+
+TEST_F(AssetManagerTest, get_two_asset_types_test)
+{
+    auto asset1 = AssetManager::instance().get<DummyAsset>("asset_one");
+    auto asset2 = AssetManager::instance().get<SecondDummyAsset>("asset_two");
+    ASSERT_NE(asset1, nullptr);
+    ASSERT_NE(asset2, nullptr);
+    EXPECT_EQ(asset1->name, "asset_one");
+	EXPECT_EQ(asset2->name, "asset_two");
+}
+
+TEST_F(AssetManagerTest, unload_asset_test) 
+{
     auto asset = AssetManager::instance().get<DummyAsset>("temp_path");
     AssetManager::instance().unload<DummyAsset>("temp_path");
 
     EXPECT_FALSE(AssetManager::instance().is_loaded<DummyAsset>("temp_path"));
 }
 
+TEST_F(AssetManagerTest, is_loaded_test)
+{
+    AssetManager::instance().get<DummyAsset>("check_path");
+    EXPECT_TRUE(AssetManager::instance().is_loaded<DummyAsset>("check_path"));
+}
 
+TEST_F(AssetManagerTest, nonexistence_is_not_loaded_test)
+{
+    EXPECT_FALSE(AssetManager::instance().is_loaded<DummyAsset>("nonexistent_asset_path"));
+}
+
+TEST_F(AssetManagerTest, clear_assetstore_test)
+{
+    AssetManager::instance().get<DummyAsset>("clear_path");
+    AssetManager::instance().clear<DummyAsset>();
+    
+	EXPECT_FALSE(AssetManager::instance().is_loaded<DummyAsset>("clear_path"));
+}
+
+TEST_F(AssetManagerTest, clear_assetstore_ignores_other_stores_test)
+{
+    AssetManager::instance().get<DummyAsset>("dummy_path");
+    AssetManager::instance().get<SecondDummyAsset>("second_dummy_path");
+    
+    AssetManager::instance().clear<DummyAsset>();
+    
+    EXPECT_FALSE(AssetManager::instance().is_loaded<DummyAsset>("dummy_path"));
+	EXPECT_TRUE(AssetManager::instance().is_loaded<SecondDummyAsset>("second_dummy_path"));
+}
+
+TEST_F(AssetManagerTest, clear_all_assets_test) 
+{
+    AssetManager::instance().get<DummyAsset>("clear_path");
+    AssetManager::instance().clear_all();
+    
+    EXPECT_FALSE(AssetManager::instance().is_loaded<DummyAsset>("clear_path"));
+}
 
