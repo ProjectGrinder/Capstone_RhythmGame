5b65df4c8db80951c460fc78bc9fa958cf1cdf61
Summation <wachirawichkanil@gmail.com>
2025-06-01 10:25:00 +0700

    feat(input) - mutex on input

--- include/system/input.h
+++ include/system/input.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include "maths/vector2.h"
+#include <mutex>
 
 namespace System
 {
@@ -10,7 +11,19 @@ namespace System
     private:
         std::array<uint8_t, 256> _keyboard_state{};
         Math::Vector2<uint16_t> mouse_position;
+
+        // This may be a REALLY dumb idea
+        std::mutex _mutex;
+
         Input() {};
+
+        // Helper for said REALLY dumb idea
+        template<typename Func>
+        static auto with_input_lock(Func&& func) {
+            auto& instance = Input::instance();
+            std::lock_guard<std::mutex> lock(instance._mutex);
+            return func(instance);
+        }
     public:
         static Input& instance();
         static void set_key_down(uint8_t vk_code);
--- src/engine/input.cpp
+++ src/engine/input.cpp
@@ -12,26 +12,51 @@ Input& Input::instance()
 
 void Input::set_key_down(uint8_t vk_code)
 {
-    Input::instance()._keyboard_state[vk_code] = 1;
+    with_input_lock
+    ([vk_code](Input& instance) 
+        {
+            instance._keyboard_state[vk_code] = 1;
+        }
+    );
 }
 
 void Input::set_key_up(uint8_t vk_code)
 {
-    Input::instance()._keyboard_state[vk_code] = 0;
+    with_input_lock
+    ([vk_code](Input& instance) 
+        {
+            instance._keyboard_state[vk_code] = 0;
+        }
+    );
 }
 
-bool Input::is_key_down(uint8_t vk_code) 
+bool Input::is_key_down(uint8_t vk_code)
 {
-    return (Input::instance()._keyboard_state[vk_code]);
+    return with_input_lock
+    ([vk_code](Input& instance) 
+        {
+            return instance._keyboard_state[vk_code];
+        }
+    );
 }
 
 void Input::set_mouse_position(uint16_t x, uint16_t y)
 {
-    Input::instance().mouse_position.x = x;
-    Input::instance().mouse_position.y = y;
+    with_input_lock
+    ([x, y](Input& instance) 
+        {
+            instance.mouse_position.x = x;
+            instance.mouse_position.y = y;
+        }
+    );
 }
 
-Math::Vector2<uint16_t> Input::get_mouse_position() 
-{ 
-    return (Input::instance().mouse_position);
+Math::Vector2<uint16_t> Input::get_mouse_position()
+{
+    return with_input_lock
+    ([](Input& instance) 
+        {
+        return instance.mouse_position;
+        }
+    );
 }
