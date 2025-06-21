bbe8238d6e179e53ac824ad9727efc848dc43f8f
Summation <wachirawichkanil@gmail.com>
2025-06-21 11:12:56 +0700

    feat(test) - add new ecs test

--- test/ecs_test.cpp
+++ test/ecs_test.cpp
@@ -3,32 +3,97 @@
 
 struct test_component
 {
+    test_component(): value(0){}
+    explicit test_component(const int value) : value(value)
+    {}
     int value;
 };
 
 struct test_component_2
 {
+    test_component_2(): value(0){}
+    explicit test_component_2(const int value) : value(value)
+    {}
     int value;
 };
 
 void test_system([[maybe_unused]] System::pid id, test_component& comp)
 {
-    comp.value = 1;
+    comp.value += 1;
 }
 
 void test_system_2([[maybe_unused]] System::pid id, test_component& comp)
 {
-    comp.value = 2;
+    comp.value *= 2;
 }
 
 TEST(ECS, add_component_test)
 {
     using TestResource = System::ResourceManager<1000, test_component>;
     TestResource resource;
-    test_component comp;
     const System::pid id = resource.add_process();
-    comp.value = 1;
-    auto pool = resource.query<test_component>();
-    pool.add(id, comp);
-    EXPECT_EQ(pool.get(id).value, 1);
+    resource.add_resource<test_component>(id, test_component{1});
+    EXPECT_EQ(resource.query<test_component>().get(id).value, 1);
+    EXPECT_TRUE(resource.query<test_component>().has(id));
+}
+
+TEST(ECS, add_multiple_components_test)
+{
+    using TestResource = System::ResourceManager<1000, test_component>;
+    TestResource resource;
+    const System::pid id_1 = resource.add_process();
+    resource.add_resource<test_component>(id_1, test_component{1});
+    const System::pid id_2 = resource.add_process();
+    resource.add_resource<test_component>(id_2, test_component{2});
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 1);
+    EXPECT_EQ(resource.query<test_component>().get(id_2).value, 2);
+}
+
+TEST(ECS, layer_components_test)
+{
+    using TestResource = System::ResourceManager<1000, test_component, test_component_2>;
+    TestResource resource;
+    const System::pid id_1 = resource.add_process();
+    resource.add_resource<test_component>(id_1, test_component{1});
+    resource.add_resource<test_component_2>(id_1, test_component_2{2});
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 1);
+    EXPECT_EQ(resource.query<test_component_2>().get(id_1).value, 2);
+}
+
+TEST(ECS, delete_resource_test)
+{
+    using TestResource = System::ResourceManager<1000, test_component, test_component_2>;
+    TestResource resource;
+    const System::pid id_1 = resource.add_process();
+    resource.add_resource<test_component>(id_1, test_component{1});
+    resource.add_resource<test_component_2>(id_1, test_component_2{2});
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 1);
+    EXPECT_EQ(resource.query<test_component_2>().get(id_1).value, 2);
+    resource.delete_entity(id_1);
+    EXPECT_FALSE(resource.query<test_component>().has(id_1));
+    EXPECT_FALSE(resource.query<test_component_2>().has(id_1));
+}
+
+TEST(ECS, system_run_test)
+{
+    using TestResource = System::ResourceManager<1000, test_component>;
+    TestResource resource;
+    const System::pid id_1 = resource.add_process();
+    resource.add_resource<test_component>(id_1, test_component{1});
+    System::TaskManager<TestResource, test_system> task_manager(resource);
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 1);
+    task_manager.run_all();
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 2);
+}
+
+TEST(ECS, system_sequence_test)
+{
+    using TestResource = System::ResourceManager<1000, test_component>;
+    TestResource resource;
+    const System::pid id_1 = resource.add_process();
+    resource.add_resource<test_component>(id_1, test_component{1});
+    System::TaskManager<TestResource, test_system, test_system_2> task_manager(resource);
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 1);
+    task_manager.run_all();
+    EXPECT_EQ(resource.query<test_component>().get(id_1).value, 4); // (1+1)*2
 }
