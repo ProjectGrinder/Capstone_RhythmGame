0aeec0a03792fb11f3da9de8d677c5d6ce6880d0
Nack <77537263+nack098@users.noreply.github.com>
2025-09-23 09:59:15 +0700

    refactor(renderer): split definition and implementation

---
+++ src/engine/renderer.cpp
@@ -0,0 +1,12 @@
+#include "system/renderer.h"
+
+/* Constructor */
+System::Renderer::RenderItem::RenderItem(Render2D render_2d)
+{
+
+}
+
+System::Renderer::RenderItem::RenderItem(Render3D render_3d)
+{
+
+}
