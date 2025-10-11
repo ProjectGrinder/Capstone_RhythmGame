8ffe2efe6bd89c141d4dccce9a5e20cbb760d72d
Nack <77537263+nack098@users.noreply.github.com>
2025-10-11 22:20:47 +0700

    fix(scene_manager) - memory leak

--- src/scene/scene_manager.cpp
+++ src/scene/scene_manager.cpp
@@ -1,7 +1,60 @@
 #include "scene.h"
 
-Scene::SceneManager &Scene::SceneManager::instance()
+namespace Scene
 {
-    static Scene::SceneManager instance;
-    return(instance);
-}
+    SceneManager &SceneManager::instance()
+    {
+        static SceneManager instance;
+        return (instance);
+    }
+
+    void SceneManager::init(System::Renderer::VertexGeneratorQueue<System::Config::VertexQueueSize> *queue)
+    {
+        instance()._generator_queue = queue;
+    }
+
+    void SceneManager::update()
+    {
+        std::visit(
+                []<typename S>([[maybe_unused]] S &&scene)
+                {
+                    if constexpr (!std::is_same_v<std::decay_t<S>, std::monostate>)
+                    {
+                        using SceneType = std::decay_t<S>;
+                        auto &manager = std::any_cast<typename SceneType::TaskManager &>(instance()._current_manager);
+                        manager.run_all();
+
+                        if (instance()._generator_queue == nullptr)
+                        {
+                            LOG_DEBUG(
+                                    "Warn: Vertex queue generator didn't get initialize. Please check if you have "
+                                    "called init "
+                                    "function");
+                            return;
+                        }
+                        instance()._generator_queue->write(manager.get_rm());
+                    }
+                    else
+                    {
+                        LOG_DEBUG("Warn: No scene loaded during update");
+                    }
+                },
+                instance()._current_scene_template);
+    }
+
+    void SceneManager::clean_up()
+    {
+        LOG_DEBUG("Info: Cleaning up...");
+        auto &scene_template = instance()._current_scene_template;
+        instance()._current_manager.reset();
+        std::visit(
+                []<typename S>(S &&scene)
+                {
+                    if constexpr (!std::is_same_v<std::decay_t<S>, std::monostate>)
+                    {
+                        scene.Exit();
+                    }
+                },
+                scene_template);
+    }
+} // namespace Scene
