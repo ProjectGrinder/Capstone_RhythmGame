c0653835a3f08872d892600528e7da78ff9e2ac7
nack098 <nack098gamer@gmail.com>
2025-12-02 00:35:55 +0700

    feat(scene) - connect scene

---
+++ src/windows/scenes/scenes_api.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <windows.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    /* This scene will run on seperate threads */
+    typedef struct SceneAPI SceneAPI;
+    typedef void *SceneManagerHandler;
+
+    HRESULT scene_manager_init(SceneManagerHandler *api);
+    void scene_manager_update(SceneManagerHandler *api);
+    void scene_manager_cleanup(const SceneManagerHandler *api);
+#ifdef __cplusplus
+}
+#endif
