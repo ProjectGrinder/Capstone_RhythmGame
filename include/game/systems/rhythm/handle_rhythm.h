46de8c579e98eb71833aa2b7ad3690cdc2cf6a2e
Pooh5821 <pooh5821ingkanunt@gmail.com>
2026-01-08 22:11:51 +0700

    feat(battle) - implement handle rhythm

--- include/game/systems/rhythm/handle_rhythm.h
+++ include/game/systems/rhythm/handle_rhythm.h
@@ -26,6 +26,55 @@ namespace Game::Rhythm
         // This system should check the input, remove a note if it was hit correctly, and update score/hp/etc. correctly
         // If you need to adjust components or data structures you can ask what structures implement what data to
         // Midfield
-        // TODO: Pooh part please implement
+        for (auto &[id, comp] : input_query)
+        {
+            if (comp.get<KeyInput>().input1 == true)
+            {
+                HandleJudgement(0, syscall, battle_query, input_query, note_query);
+            }
+            if (comp.get<KeyInput>().input2 == true)
+            {
+                HandleJudgement(1, syscall, battle_query, input_query, note_query);
+            }
+            if (comp.get<KeyInput>().input3 == true)
+            {
+                HandleJudgement(2, syscall, battle_query, input_query, note_query);
+            }
+            if (comp.get<KeyInput>().input4 == true)
+            {
+                HandleJudgement(3, syscall, battle_query, input_query, note_query);
+            }
+        }
+    }
+
+    template<typename T>
+    void HandleJudgement(
+        int lane_num, // assume that first lane is lane 0
+        [[maybe_unused]] T &syscall,
+        [[maybe_unused]] System::ECS::Query<Battle::BattleState> &battle_query,
+        System::ECS::Query<Rhythm::KeyInput> &input_query,
+        System::ECS::Query<Rhythm::Lane, Rhythm::Timing> &note_query)
+    {
+        for (auto &[id2, comp2] : note_query)
+        {
+            if (comp2.get<Lane>().lane == lane_num)
+            {
+                if (comp2.get<Timing>().timing >= -50 && comp2.get<Timing>().timing <= 50)
+                {
+                    battle_query.front().get<Battle::BattleState>().judgement_count.perfect_count += 1;
+                    syscall.remove_entity(id2);
+                }
+                else if (comp2.get<Timing>().timing >= -75 && comp2.get<Timing>().timing <= 75)
+                {
+                    battle_query.front().get<Battle::BattleState>().judgement_count.great_count += 1;
+                    syscall.remove_entity(id2);
+                }
+                else if (comp2.get<Timing>().timing >= -100 && comp2.get<Timing>().timing <= 100)
+                {
+                    battle_query.front().get<Battle::BattleState>().judgement_count.fine_count += 1;
+                    syscall.remove_entity(id2);
+                }
+            }
+        }
     }
 } // namespace Game::Rhythm
