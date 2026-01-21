28631206767f560f29105d492f2cb60dc47b5368
lolpopih845 <rangsiman7999@gmail.com>
2026-01-21 12:14:13 +0700

    fix(Particle) - Add fade option (resume when renderer finished)

--- include/game/components/bullethell/particle.h
+++ include/game/components/bullethell/particle.h
@@ -2,12 +2,20 @@
 
 namespace Game::BulletHell
 {
+    enum ParticleDestroyEffect
+    {
+        Fade,
+        Shrink,
+        Disappear,
+        Custom
+    };
     struct Particle
     {
         int lifetime;
-        Particle() : lifetime(0)
+        ParticleDestroyEffect destroy_effect;
+        explicit Particle(const int lifetime) : lifetime(lifetime), destroy_effect(Disappear)
         {}
-        explicit Particle(const int lifetime) : lifetime(lifetime)
+        explicit Particle(const int lifetime, const ParticleDestroyEffect destroy_effect) : lifetime(lifetime), destroy_effect(destroy_effect)
         {}
     };
 }
