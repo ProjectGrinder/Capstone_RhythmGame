2b02970b21d8118fdb34a4c6b78803efc7b7f817
Pooh5821 <pooh5821ingkanunt@gmail.com>
2026-01-23 17:19:22 +0700

    fix(rhythm) - fix handle miss notes

--- include/game/systems/rhythm/handle_miss_note.h
+++ include/game/systems/rhythm/handle_miss_note.h
@@ -5,13 +5,16 @@
 namespace Game::Rhythm
 {
     template<typename T>
-    void HandleMissNote(T &syscall, System::ECS::Query<Lane, NoteSpeed, Timing> &query, System::ECS::Query<Lane, NoteSpeed, Timing, TimingEnd> &query2, System::ECS::Query<Battle::BattleState> &query3)
+    void HandleMissNote(
+        T &syscall,
+        System::ECS::Query<Lane, NoteSpeed, Timing, TimingEnd> &note_query,
+        System::ECS::Query<Battle::BattleState> &battle_query)
     {
 
-        if (query3.begin() == query3.end())
+        if (battle_query.begin() == battle_query.end())
             return;
 
-        if (query3.front().get<Battle::BattleState>().current_phase != Battle::CurrentPhase::RHYTHM)
+        if (battle_query.front().get<Battle::BattleState>().current_phase != Battle::CurrentPhase::RHYTHM)
         {
             return;
         }
@@ -19,24 +22,22 @@ namespace Game::Rhythm
         constexpr auto miss_timing = 100;
         auto &current_timing = query3.front().get<Battle::BattleState>().clock_time;
 
-        for (auto &[id, comps] : query)
+        for (auto &[id, comps] : note_query)
         {
             if (comps.get<Timing>().timing - current_timing < -1 * miss_timing)
             {
-                query3.front().get<Battle::BattleState>().judgement_count.miss_count += 1;
+                if (comps.get<TimingEnd>().timing_end == 0)
+                {
+                    battle_query.front().get<Battle::BattleState>().judgement_count.miss_count += 1;
+                }
+                else
+                {
+                    battle_query.front().get<Battle::BattleState>().judgement_count.miss_count += 2;
+                }
                 syscall.remove_entity(id);
             }
-        }
-
-        for (auto &[id, comps] : query2)
         // Handle miss for an entire hold note, which counts as two misses (start and end)
         // If the note is held but released too early, it should be handled in handle_rhythm.h to be checked along with other judgements
-        {
-            if (comps.get<Timing>().timing - current_timing < -1 * miss_timing)
-            {
-                query3.front().get<Battle::BattleState>().judgement_count.miss_count += 2;
-                syscall.remove_entity(id);
-            }
         }
     }
 }
