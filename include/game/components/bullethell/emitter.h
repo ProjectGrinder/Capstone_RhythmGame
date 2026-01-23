165fd12f0436eb4ab057fdfa7a0be129c75b06eb
lolpopih845 <rangsiman7999@gmail.com>
2026-01-23 20:40:19 +0700

    Emitter To be implement

--- include/game/components/bullethell/emitter.h
+++ include/game/components/bullethell/emitter.h
@@ -7,65 +7,74 @@ namespace Game::BulletHell
 {
     struct BoomEmitter
     {
-        Particle particle;
-        float rotation;
+        Bullet particle;
+        float delay;
         float scale;
         float sustain_time;
-        explicit BoomEmitter(const Particle particle, const float rotation, const float scale, const float sustain_time) :
-            particle(particle), rotation(rotation), scale(scale), sustain_time(sustain_time)
+        explicit BoomEmitter(const Bullet particle, const float delay, const float scale, const float sustain_time) :
+            particle(particle), delay(delay), scale(scale), sustain_time(sustain_time)
         {}
     };
 
     struct LaserEmitter
     {
-        Particle particle;
+        Bullet particle;
+        float delay;
         float rotation;
         float width;
         float length;
         float sustain_time;
         float speed;
-        explicit LaserEmitter(const Particle particle, const float rotation, const float width, const float length, const float sustain_time, const float speed) :
-            particle(particle), rotation(rotation), width(width), length(length), sustain_time(sustain_time), speed(speed)
+        explicit LaserEmitter(const Bullet particle, const float delay, const float rotation, const float width, const float length, const float sustain_time, const float speed) :
+            particle(particle), delay(delay), rotation(rotation), width(width), length(length), sustain_time(sustain_time), speed(speed)
         {}
     };
 
     struct RingEmitter
     {
         Particle particle;
+        float delay;
         float rotation;
         float width;
         float length;
         float fire_delay;
         int bullet_quantity;
-        explicit RingEmitter(const Particle particle, const float rotation, const float width, const float length, const float fire_delay, const int bullet_quantity) :
-            particle(particle), rotation(rotation), width(width), length(length), fire_delay(fire_delay), bullet_quantity(bullet_quantity)
+
+        int bullet_fired = 0;
+        explicit RingEmitter(const Particle particle, const float delay, const float rotation, const float width, const float length, const float fire_delay, const int bullet_quantity) :
+            particle(particle), delay(delay), rotation(rotation), width(width), length(length), fire_delay(fire_delay), bullet_quantity(bullet_quantity)
         {}
     };
 
     struct RandomEmitter
     {
         Particle particle;
+        float delay;
         float angle_range;
         float center_rotation;
         float width;
         float length;
         float fire_delay;
         int bullet_quantity;
-        explicit RandomEmitter(const Particle particle, const float angle_range, const float center_rotation, const float width, const float length, const float fire_delay, const int bullet_quantity) :
-            particle(particle), angle_range(angle_range), center_rotation(center_rotation), width(width), length(length), fire_delay(fire_delay), bullet_quantity(bullet_quantity)
+        explicit RandomEmitter(const Particle particle, const float delay, const float angle_range, const float center_rotation, const float width, const float length, const float fire_delay, const int bullet_quantity) :
+            particle(particle), delay(delay), angle_range(angle_range), center_rotation(center_rotation), width(width), length(length), fire_delay(fire_delay), bullet_quantity(bullet_quantity)
         {}
     };
 
     struct SpreadEmitter
     {
         Particle particle;
+        float delay;
         float angle_range;
         float center_rotation;
         float width;
         float length;
+        float fire_delay;
         int bullet_quantity;
-        explicit SpreadEmitter(const Particle particle, const float angle_range, const float center_rotation, const float width, const float length, const int bullet_quantity) :
-            particle(particle), angle_range(angle_range), center_rotation(center_rotation), width(width), length(length), bullet_quantity(bullet_quantity)
+
+        int bullet_fired = 0;
+        explicit SpreadEmitter(const Particle particle, const float delay, const float angle_range, const float center_rotation, const float width, const float length, const float fire_delay, const int bullet_quantity) :
+            particle(particle), delay(delay), angle_range(angle_range), center_rotation(center_rotation), width(width), length(length), fire_delay(fire_delay), bullet_quantity(bullet_quantity)
         {}
     };
 }
--- include/game/systems/bullethell/emitter_system.h
+++ include/game/systems/bullethell/emitter_system.h
@@ -13,6 +13,51 @@ namespace Game::BulletHell
     template <typename T>
     void BoomEmitterSystem([[maybe_unused]] T &syscall, System::ECS::Query<BoomEmitter>& query, System::ECS::Query<Battle::BattleState> &query2)
     {
+        // Idk : 1. Create More Comp -> Emitter obsolete , 2. Alternate Workaround -> Bad practice
+    }
 
+    template <typename T>
+    void LaserEmitterSystem([[maybe_unused]] T &syscall, System::ECS::Query<LaserEmitter>& query, System::ECS::Query<Battle::BattleState> &query2)
+    {
+        // Idk : 1. Create More Comp -> Emitter obsolete , 2. Alternate Workaround -> Bad practice
+    }
+
+    template <typename T>
+    void RingEmitterSystem([[maybe_unused]] T &syscall, System::ECS::Query<RingEmitter>& query, System::ECS::Query<Battle::BattleState> &query2)
+    {
+        // TODO : Using Particle Prefab
+        //  1. Assign MoveRotation = emitter.rotation
+        //  2. For each particle
+        //      2.1 calculate position of particle relative to width & height & bullet_quantity
+        //      2.2 assign moveRot = rotation + bullet_fired*360/bullet_quantity
+        //      2.2 bullet_fired++
+        //      2.3 if bullet_fired = quantity -> destroy
+        //      2.4 add delay for fire_delay
+    }
+
+    template <typename T>
+    void RandomEmitterSystem([[maybe_unused]] T &syscall, System::ECS::Query<RingEmitter>& query, System::ECS::Query<Battle::BattleState> &query2)
+    {
+        // TODO : Using Particle Prefab
+        //  1. Assign MoveRotation = emitter.rotation
+        //  2. For each particle
+        //      2.1 random position of particle with width & height
+        //      2.2 assign moveRot = center_rot + random from angle_range
+        //      2.2 quantity--
+        //      2.3 if quantity = 0 -> destroy
+        //      2.4 add delay for fire_delay
+    }
+
+    template <typename T>
+    void SpreadEmitterSystem([[maybe_unused]] T &syscall, System::ECS::Query<RingEmitter>& query, System::ECS::Query<Battle::BattleState> &query2)
+    {
+        // TODO : Using Particle Prefab
+        //  1. Assign MoveRotation = emitter.rotation
+        //  2. For each particle
+        //      2.1 calculate position of particle relative to width & height & bullet_quantity
+        //      2.2 assign moveRot = rotation + smth
+        //      2.2 bullet_fired++
+        //      2.3 if bullet_fired = quantity -> destroy
+        //      2.4 add delay for fire_delay
     }
 }
