1b6b9e5185591763d53bb4ff914d5ed985182fbd
ZigmaZero <wachirawichkanil@gmail.com>
2026-02-24 14:33:57 +0700

    Revert "test(intent-storage) - set up test"
    This reverts commit 61c970c76d8ea26750686a9f5d9a0e1a39b91d15.

--- include/game/systems/render/set_camera.h
+++ include/game/systems/render/set_camera.h
@@ -5,9 +5,6 @@ namespace Game::Render
     template<typename T>
     void set_camera([[maybe_unused]] T &syscall, System::ECS::Query<Game::Render::Camera2D> &query)
     {
-        if (query.begin() == query.end())
-            return;
-
         const auto &[offsetX, offsetY, scaleX, scaleY, rotation, zoom] = query.front().get<System::Render::Camera>();
         // ReSharper disable once CppUseStructuredBinding
         auto &intent_camera = System::Render::IntentStorage::get_camera();
