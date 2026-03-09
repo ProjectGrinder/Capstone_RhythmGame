010c5e2f737cc6a87c869972c82d6791682487af
Pooh5821 <pooh5821ingkanunt@gmail.com>
2026-03-09 13:51:02 +0700

    fix(rhythm) - change judge range for demo test

--- include/game/systems/rhythm/test_rhythm.h
+++ include/game/systems/rhythm/test_rhythm.h
@@ -9,9 +9,9 @@ namespace Game::Rhythm
         const int current_timing)
     {
         const auto time_diff = current_timing - note_timing;
-        constexpr auto perfect_judge = 50;
-        constexpr auto great_judge = 75;
-        constexpr auto fine_judge = 100;
+        constexpr auto perfect_judge = 500;
+        constexpr auto great_judge = 750;
+        constexpr auto fine_judge = 1000;
         if (time_diff > -1 * perfect_judge && time_diff < perfect_judge)
         {
             LOG_INFO("Timing %d: Perfect (Diff: %d ms)", note_timing, time_diff);
@@ -45,23 +45,23 @@ namespace Game::Rhythm
         if (input_query.front().get<KeyInput>().key2_pressed)
         {
             auto current_timing = battle_query.front().get<Battle::BattleState>().clock_time / 1000;
-            if (current_timing < 5100)
+            if (current_timing < 6000)
             {
                 check_judgement(5000, current_timing);
             }
-            else if (current_timing < 10100)
+            else if (current_timing < 11000)
             {
                 check_judgement(10000, current_timing);
             }
-            else if (current_timing < 15100)
+            else if (current_timing < 16000)
             {
                 check_judgement(15000, current_timing);
             }
-            else if (current_timing < 20100)
+            else if (current_timing < 21000)
             {
                 check_judgement(20000, current_timing);
             }
-            else if (current_timing < 25100)
+            else if (current_timing < 26000)
             {
                 check_judgement(25000, current_timing);
             }
