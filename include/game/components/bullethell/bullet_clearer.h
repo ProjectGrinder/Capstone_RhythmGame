1cc234c83248ece8e88c994ebe1cabd52fd26a49
lolpopih845 <rangsiman7999@gmail.com>
2026-03-18 17:14:47 +0700

    Feat(BulletClearer) - Area that destroy bullet

---
+++ include/game/components/bullethell/bullet_clearer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "game/utils/physics_util.h"
+
+namespace Game::BulletHell
+{
+    struct BulletClearer
+    {
+        float max_size;
+        int speed;
+        int lifetime = UNASSIGNED;
+        BulletClearer() : max_size(10), speed(1) {}
+        BulletClearer(const float size, const int speed) : max_size(size), speed(speed) {}
+    };
+} // namespace Game::BulletHell
